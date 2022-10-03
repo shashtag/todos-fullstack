4b6cca5cff6f326490862910bd5c692eb6e0a43c
Shashwat Gupta <shashwatsatna@gmail.com>
2022-10-03 16:37:57 +0530

    ⚰️ clear imports

--- src/app/src/Context/AppContext.js
+++ src/app/src/Context/AppContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useReducer } from "react";
+import React, { createContext, useReducer } from "react";
 import { initialData } from "../Data/initialData";
 
 export const AppContext = createContext();
